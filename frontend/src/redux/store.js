c8d171c2facdec97f1fedca9570b5095311a1c55
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2024-07-19 13:39:59 +0530

    Make redux store persisted using redux-persistor and storing state data in local storage

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { userReducer } from "./slices/user.slice.js";
 import { notificationReducer } from "./slices/notification.slice.js";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  notification: notificationReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-    notification: notificationReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+export const persister = persistStore(store);
