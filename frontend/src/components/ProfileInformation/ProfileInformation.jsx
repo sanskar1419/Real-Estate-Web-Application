4d9069c3f5f0c3fb04c89783f905ff4b6c4f2aeb
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2024-07-27 20:11:57 +0530

    Uploading file to cloud firestore and show it to users

--- frontend/src/components/ProfileInformation/ProfileInformation.jsx
+++ frontend/src/components/ProfileInformation/ProfileInformation.jsx
@@ -1,8 +1,15 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { getCurrentUser } from "../../redux/slices/user.slice";
 import SettingsMenu from "../SettingsMenu/SettingsMenu";
 import styles from "./ProfileInformation.module.css";
+import { app } from "../../firebase";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 
 export default function ProfileInformation() {
   const profilePictureRef = useRef(null);
@@ -12,11 +19,40 @@ export default function ProfileInformation() {
     dob: "1999-12-19",
     phoneNumber: 8259990668,
     address: "Agartala, Tripura India",
+    avatar: currentUser.avatar,
   });
   const [showUpdateButton, setShowUpdateButton] = useState(false);
   const [file, setFile] = useState(undefined);
+  const [fileUploadPercentage, setFileUploadPercentage] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
 
-  console.log(file);
+  useEffect(() => {
+    if (file) handleFileUpload(file);
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const filename = new Date().getTime() + file.name;
+    const storageRef = ref(storage, filename);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFileUploadPercentage(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, avatar: downloadURL })
+        );
+      }
+    );
+  };
 
   const handleFormData = (e) => {
     setFormData({
@@ -27,27 +63,29 @@ export default function ProfileInformation() {
 
   return (
     <div
-      className={`absolute bottom-[-15%] w-[90%] bg-slate-900 p-4 flex items-center justify-center gap-3 h-[30vh] ${styles.profileContainer}`}
-      s
+      className={`absolute bottom-[-15%] w-[90%] bg-slate-900 p-4 flex items-center justify-center gap-3 h-[30vh] ${styles.profileContainer} min-h-[35vh]`}
     >
       <SettingsMenu
         showUpdateButton={showUpdateButton}
         setShowUpdateButton={setShowUpdateButton}
       />
-      <input
-        type="file"
-        accept="image/*"
-        ref={profilePictureRef}
-        hidden
-        onChange={(e) => setFile(e.target.files[0])}
-      />
-      <div className={`${styles.profilePhotoContainer}`}>
-        <img
-          alt="profile"
-          src={currentUser.avatar}
-          className="w-full h-full"
-          onClick={() => profilePictureRef.current.click()}
+
+      <div className="flex items-center justify-center mr-3 flex-col">
+        <input
+          type="file"
+          accept="image/*"
+          ref={profilePictureRef}
+          hidden
+          onChange={(e) => setFile(e.target.files[0])}
         />
+        <div className={`${styles.profilePhotoContainer}`}>
+          <img
+            alt="profile"
+            src={formData.avatar}
+            className="w-full h-full"
+            onClick={() => profilePictureRef.current.click()}
+          />
+        </div>
       </div>
       <div className="border-r border-gray-300 w-[20%] ">
         <div className=" text-xl font-bold">Sanskar Gupta</div>
