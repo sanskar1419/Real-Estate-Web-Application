3b9a19e62cf8ad04c3c5cc390ece859fb3ac5f81
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2024-08-15 13:53:29 +0530

    Update properties styling in profile page

--- frontend/src/components/Property/Property.jsx
+++ frontend/src/components/Property/Property.jsx
@@ -29,7 +29,6 @@ export default function Property() {
       dispatch(notificationAction.setError(error.message));
     }
   };
-  console.log(userProperties);
 
   return (
     <div className="w-[90%] xl:w-[65%] lg:w-[55%] md:w-[80%] cursive lg:max-h-full text-[#adbbda] flex flex-col relative">
@@ -42,12 +41,6 @@ export default function Property() {
           <img alt="arrow" src={downArrow} className="w-5 cursor-pointer" />
         </div>
       )}
-      {/* {showProperty && userProperties.length > 0 && (
-        <div className="flex items-center gap-3 justify-center fixed w-full top-0">
-          <div className="text-2xl text-first">Hide Properties</div>
-          <img alt="arrow" src={upArrow} className="w-5 cursor-pointer" />
-        </div>
-      )} */}
 
       {showProperty && userProperties.length == 0 && (
         <div className="flex lg:max-h-[95%] place-items-center third-color cursive text-[#adbbda] px-6 py-24 sm:py-32 lg:px-8 rounded-2xl shadow-inner shadow-black items-center justify-center">
@@ -85,20 +78,20 @@ export default function Property() {
           <div className="flex gap-2 w-full flex-col lg:max-h-[90%] overflow-y-auto no-scrollbar items-center">
             {userProperties.map((img, index) => (
               <div
-                className={`w-[100%] lg:w-[90%] p-3 flex items-center justify-between second-color rounded-2xl shadow-inner shadow-black ${styles.propertyBar} gap-3`}
+                className={`w-[90%] p-3 flex items-center second-color rounded-2xl shadow-inner shadow-black ${styles.propertyBar} gap-3`}
               >
                 <img
                   src={img.imageUrls[0]}
                   className="w-10 h-10 md:w-14 md:h-14  sm:w-12 sm:h-12 rounded-full"
                 />
-                <div className="w-[40%] hidden xl:block min-[1020px]:hidden sm:block overflow-clip">
+                <Link className="w-[70%] sm:block truncate hover:underline">
                   {img.propertyName}
-                </div>
+                </Link>
                 <button className="btn btn-outline btn-ghost btn-sm rounded-lg text-xs md:text-sm">
                   Delete
                 </button>
                 <button className="btn btn-outline btn-ghost btn-sm rounded-lg text-xs md:text-sm">
-                  View Details
+                  Edit
                 </button>
               </div>
             ))}
